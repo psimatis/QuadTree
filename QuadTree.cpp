72fc06862032a2ef8cb46b26529f0261967a7786
psimatis <psimatis@connect.ust.hk>
2021-09-26 17:15:44 +0800

    minor code clean up

--- QuadTree.cpp
+++ QuadTree.cpp
@@ -95,12 +95,12 @@ void QuadTreeNode::rangeQuery(Record q, vector<float> &resultItemsIds, map<strin
     if (isLeaf()) {
         if (intersects(q)) {
             stats["leaf"]++;
-            /*for (auto r : data) {
+            for (auto r : data) {
                 if (q.intersects(r)) {
                     //cout << r.id << " " << r.box[XLOW] << " " << r.box[YLOW] << endl;
                     resultItemsIds.push_back(r.id);
                 }
-            }*/
+            }
         }
         return;
     } else {
@@ -201,7 +201,7 @@ void QuadTreeNode::kNNQuery(array<float, 2> q, map<string, double> &stats, int k
         } else
             break;
     }
-
+	// prints the results
     /*while (!knnPts.empty()) {
         cout << knnPts.top().pt[0] << " " << knnPts.top().pt[1] << " dist: " << knnPts.top().dist
              << " id:" << knnPts.top().id << endl;
@@ -235,8 +235,7 @@ void QuadTreeNode::count(int &p, int &d, int &dpc, int &pc) {
 }
 
 void QuadTreeNode::calculateSize(int &s) {
-    s += sizeof(int)      // height and capacity
-         + sizeof(float) * 4; // rectangle
+    s += sizeof(int) + sizeof(float) * 4; // height  and rectangle
     if (isLeaf())
         return;
     else
